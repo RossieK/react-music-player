80288352847dc63755c489de4fe555049df13e72
RossieK <rosi.kirova96@gmail.com>
2021-03-03 23:02:44 +0200

    Add base components - player and song

---
+++ src/components/Player.js
@@ -0,0 +1,14 @@
+const Player = () => {
+    return (
+        <div className="player">
+            <div className="time-control">
+                <p>Start Time</p>
+                <input type="range" />
+                <p>End Time</p>
+            </div>
+            <div className="play-control"></div>
+        </div>
+    );
+}
+
+export default Player;
