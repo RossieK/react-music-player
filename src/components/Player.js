6ebe6bfb7f58e1f23f1a2eff15073924cced5921
RossieK <rosi.kirova96@gmail.com>
2021-03-05 22:16:03 +0200

    Add ref with current song audio to player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,16 @@
+import { useRef } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({ currentSong }) => {
+    //Ref
+    const audioRef = useRef(null);
+
+    //Event Handlers
+    const playSongHandler = () => {
+        console.log(audioRef.current);
+    }
+
     return (
         <div className="player">
             <div className="time-control">
@@ -11,9 +20,10 @@ const Player = () => {
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-                <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
             </div>
+            <audio ref={audioRef} src={currentSong.audio}></audio>
         </div>
     );
 }
